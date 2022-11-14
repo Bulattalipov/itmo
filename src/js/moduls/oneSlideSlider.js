3ef7b2f5b67d7550a3b13173bb4d1eb0eeab8758
Bulat <bulat0talipov@gmail.com>
2022-11-14 18:26:51 +0300

    hotfixes

--- src/js/moduls/oneSlideSlider.js
+++ src/js/moduls/oneSlideSlider.js
@@ -1,16 +1,23 @@
 import {
     Swiper,
-    Navigation
+    Navigation,
+    EffectFade,
+    Controller
   } from 'swiper';
   
   export default function oneSlideSlider() {
+
     const oneSlideSlider = document.querySelectorAll('.js-one-slide');
     oneSlideSlider.forEach(slider => {
-      new Swiper(slider.querySelector('.swiper'), {
+      const contentSwiper = new Swiper(slider.querySelector('.swiper'), {
         speed: 500,
         slidesPerView: 1,
         spaceBetween: 20,
-        modules: [Navigation],
+        modules: [EffectFade, Navigation, Controller],
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
         navigation: {
           nextEl: '.js-one-slide .next',
           prevEl: '.js-one-slide .prev'
@@ -21,5 +28,26 @@ import {
           },
         }
       });
+
+      const textSlider = new Swiper(slider.querySelector('.content-text__text-slider'), {
+        speed: 500,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        modules: [ EffectFade, Controller],
+        effect: 'fade',
+        autoHeight: true,
+        fadeEffect: {
+          crossFade: true
+        },
+        on: {
+          init: function (swiper) {
+            swiper.el.classList.remove("loading")
+          },
+        }
+      });
+  
+      contentSwiper.controller.control = textSlider;
+      textSlider.controller.control = contentSwiper;
+
     });
   }
