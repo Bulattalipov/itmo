fe2123990eda11721f87178fc7ed9847e3448d20
Bulat <bulat0talipov@gmail.com>
2022-11-17 18:53:38 +0300

    added changes

--- src/js/moduls/choices-select.js
+++ src/js/moduls/choices-select.js
@@ -1,12 +1,25 @@
 import Choices from "choices.js";
 
 export default function choicesSelector(){
-    const elements = document.querySelectorAll('.js-choice-select');
-    elements.forEach(elem => {
-        const choices = new Choices(elem, {
-            searchEnabled: false,
-            itemSelectText: '',
-            shouldSort: false
-        });
-    })
+    const forms = document.querySelectorAll('.filter form');
+
+    forms.forEach(form => {
+        const elements = form.querySelectorAll('.js-choice-select');
+        const btnReset = form.querySelector('.filter__footer-reset');
+
+        elements.forEach(elem => {
+            const choices = new Choices(elem, {
+                searchEnabled: false,
+                itemSelectText: '',
+                shouldSort: false
+            });
+
+            if(btnReset){
+                btnReset.addEventListener('click', function(){
+                    choices.destroy();
+                    choices.init();
+                })
+            }
+        })
+    });
 }
